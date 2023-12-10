eb56e51d955b374575bac61ea178c8f1421713c3
t-haruki122 <231205122@ccmailg.meijo-u.ac.jp>
2023-12-10 13:33:53 +0900

    Add config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+# This file contains the configuration of the bot.
+
+
+# /////////////////////////////////////////////////////////////////////////////
+
+# Your bot token here.
+TOKEN = ""
+
+# /////////////////////////////////////////////////////////////////////////////
+
+# The channel ID where the bot will send the first message.
+CHANNEL_ID = 0
+
+# /////////////////////////////////////////////////////////////////////////////
+
+
+if __name__ == "__main__":
+    print("Your TOKEN is: {}".format(TOKEN))
+    print("Your CHANNEL_ID is: {}".format(CHANNEL_ID))
+    print("Please run main.py !")
+    exit()
